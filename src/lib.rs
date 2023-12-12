0cf231c71217155007a6bfc1b32f00565d2a4d5f
David Stangl <david@david-stangl.com>
2023-12-12 14:32:03 +0100

    Implement formatting traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 //! TODO
+use std::fmt;
+use std::fmt::Binary;
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::LowerHex;
+use std::fmt::UpperHex;
 use std::ops::BitAndAssign;
 use std::ops::BitOrAssign;
 
 use num_traits::PrimInt;
 
 /// Integer that can be used as a block of bits in a bitset.
-pub trait BitBlock: PrimInt + BitAndAssign + BitOrAssign + 'static {
+pub trait BitBlock:
+    PrimInt + BitAndAssign + BitOrAssign + Binary + LowerHex + UpperHex + 'static
+{
     /// Number of bits in the block.
     const BITS: usize;
 
@@ -35,7 +44,7 @@ macro_rules! impl_bit_block {
 impl_bit_block!(u8, u16, u32, u64, u128);
 
 /// TODO
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InlineBitSet<T: BitBlock, const N: usize> {
     blocks: [T; N],
 }
@@ -96,6 +105,69 @@ impl<T: BitBlock> From<T> for InlineBitSet<T, 1> {
     }
 }
 
+impl<T: BitBlock, const N: usize> Debug for InlineBitSet<T, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "InlineBitSet([")?;
+        for (i, block) in self.blocks.into_iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+
+            // The `+ 2` accounts for the `0b` prefix
+            write!(f, "{block:#0width$b}", width = T::BITS + 2)?;
+        }
+        write!(f, "])")
+    }
+}
+
+impl<T: BitBlock, const N: usize> Display for InlineBitSet<T, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Binary::fmt(self, f)
+    }
+}
+
+impl<T: BitBlock, const N: usize> Binary for InlineBitSet<T, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "0b")?;
+        }
+
+        for block in self.blocks.iter().rev() {
+            write!(f, "{block:0width$b}", width = T::BITS)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: BitBlock, const N: usize> LowerHex for InlineBitSet<T, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "0x")?;
+        }
+
+        for block in self.blocks.iter().rev() {
+            write!(f, "{block:0width$x}", width = T::BITS / 4)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: BitBlock, const N: usize> UpperHex for InlineBitSet<T, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "0x")?;
+        }
+
+        for block in self.blocks.iter().rev() {
+            write!(f, "{block:0width$X}", width = T::BITS / 4)?;
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -151,5 +223,63 @@ mod tests {
     #[test]
     fn default() {
         assert_eq!(TestBitSet::EMPTY, TestBitSet::default());
+    }
+
+    #[test]
+    fn debug_formatting() {
+        assert_eq!(
+            "InlineBitSet([0b00000000, 0b00000000])",
+            format!("{:?}", TestBitSet::EMPTY)
+        );
+        assert_eq!(
+            "InlineBitSet([0b11111111, 0b11111111])",
+            format!("{:?}", TestBitSet::ALL)
+        );
+        assert_eq!(
+            "InlineBitSet([0b01010101, 0b10101010])",
+            format!("{:?}", TestBitSet::from([0b0101_0101, 0b1010_1010]))
+        );
+
+        assert_eq!(
+            "InlineBitSet([0b00111100])",
+            format!("{:#?}", InlineBitSet::from(0b0011_1100_u8))
+        );
+    }
+
+    #[test]
+    fn display_formatting() {
+        assert_eq!("0000000000000000", TestBitSet::EMPTY.to_string());
+        assert_eq!("1111111111111111", TestBitSet::ALL.to_string());
+        assert_eq!(
+            "1111000000001111",
+            TestBitSet::from([0b0000_1111, 0b1111_0000]).to_string()
+        );
+    }
+
+    #[test]
+    fn binary_formatting() {
+        assert_eq!("0000000000000000", format!("{:b}", TestBitSet::EMPTY));
+        assert_eq!("0b0000000000000000", format!("{:#b}", TestBitSet::EMPTY));
+        assert_eq!("1111111111111111", format!("{:b}", TestBitSet::ALL));
+        assert_eq!(
+            "1111000000001111",
+            format!("{:b}", TestBitSet::from([0b0000_1111, 0b1111_0000]))
+        );
+    }
+
+    #[test]
+    fn lower_hex_formatting() {
+        assert_eq!("0000", format!("{:x}", TestBitSet::EMPTY));
+        assert_eq!("0x0000", format!("{:#x}", TestBitSet::EMPTY));
+        assert_eq!("ffff", format!("{:x}", TestBitSet::ALL));
+        assert_eq!("e71e", format!("{:x}", TestBitSet::from([0x1e, 0xe7])));
+    }
+
+    #[test]
+    fn upper_hex_formatting() {
+        assert_eq!("0000", format!("{:X}", TestBitSet::EMPTY));
+        assert_eq!("0x0000", format!("{:#X}", TestBitSet::EMPTY));
+        assert_eq!("FFFF", format!("{:X}", TestBitSet::ALL));
+        assert_eq!("E71E", format!("{:X}", TestBitSet::from([0x1E, 0xE7])));
     }
 }
